01b84f3ddd7fd3f522ea9f7fc083cae6dba98975
Austin Drenski <austin.drenski@gmail.com>
2017-05-13 14:22:41 -0400

    Creating class structure to represent a HAR file

---
+++ HeaderArrayConverter/HeaderArrayConverter/HeaderArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace HeaderArrayConverter
+{
+    /// <summary>
+    /// Represents a Header Array (HAR) file.
+    /// </summary>
+    [PublicAPI]
+    public class HeaderArray
+    {
+        /// <summary>
+        /// Represents a Header Array (HAR) file.
+        /// </summary>
+        /// <param name="file">
+        /// The file path from which to read a HAR file.
+        /// </param>
+        public HeaderArray(string file)
+        {
+            if (file is null)
+            {
+                throw new ArgumentNullException(nameof(file));
+            }
+
+
+        }
+    }
+}
