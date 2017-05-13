a5a7fffa5fb105b1587f935ee1e77635a84ad716
Austin Drenski <austin.drenski@gmail.com>
2017-05-13 19:15:40 -0400

    .

---
+++ HeaderArrayConverter/HeaderArrayConsole/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace HeaderArrayConsole
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            const string file = "C:\\Users\\adren\\Desktop\\GTAP source\\sets.har";
+
+            byte[] bytes = File.ReadAllBytes(file);
+
+            Console.WriteLine(nameof(Encoding.ASCII));
+            Console.WriteLine(Encoding.ASCII.GetString(bytes));
+            Console.WriteLine("END");
+
+            using (BinaryReader reader = new BinaryReader(File.Open(file, FileMode.Open, FileAccess.Read)))
+            {
+                Console.WriteLine(reader.Read());
+            }
+
+            using (BinaryReader reader = new BinaryReader(File.Open(file, FileMode.Open, FileAccess.Read)))
+            {
+                Console.WriteLine(reader.ReadString());
+            }
+
+            using (BinaryReader reader = new BinaryReader(File.Open(file, FileMode.Open, FileAccess.Read)))
+            {
+                Console.WriteLine(reader.ReadInt16());
+            }
+
+            using (BinaryReader reader = new BinaryReader(File.Open(file, FileMode.Open, FileAccess.Read)))
+            {
+                Console.WriteLine(reader.ReadInt32());
+            }
+
+            using (BinaryReader reader = new BinaryReader(File.Open(file, FileMode.Open, FileAccess.Read)))
+            {
+                Console.WriteLine(reader.ReadInt64());
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
