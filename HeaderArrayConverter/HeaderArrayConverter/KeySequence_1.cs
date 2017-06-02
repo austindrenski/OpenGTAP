f3af0c853434676179f1d35365f3c79012b0ba86
Austin Drenski <austin.drenski@gmail.com>
2017-06-02 17:38:47 -0400

    .

--- HeaderArrayConverter/HeaderArrayConverter/KeySequence_1.cs
+++ HeaderArrayConverter/HeaderArrayConverter/KeySequence_1.cs
@@ -16,11 +16,30 @@ namespace HeaderArrayConverter
     [PublicAPI]
     public struct KeySequence<T> : IEnumerable<T>, IEquatable<KeySequence<T>>, IStructuralEquatable
     {
-        public static KeySequence<T> Empty { get; } = new KeySequence<T>(new T[0]);
-
+        /// <summary>
+        /// The sequence values.
+        /// </summary>
         private readonly IImmutableList<T> _values;
 
+        /// <summary>
+        /// Returns an empty <see cref="KeySequence{T}"/> with the specified type argument.
+        /// </summary>
+        public static KeySequence<T> Empty { get; } = new KeySequence<T>(new T[0]);
+        
+        /// <summary>
+        /// Gets the number of items contained in the sequence.
+        /// </summary>
         public int Count => _values.Count;
+
+        /// <summary>
+        /// Returns the value at the specified index.
+        /// </summary>
+        public T this[int index] => _values[index];
+
+        /// <summary>
+        /// Returns the values at the specified index.
+        /// </summary>
+        public IEnumerable<T> this[params int[] index] => Yield(index);
 
         public KeySequence(params T[] keys)
         {
@@ -32,26 +51,70 @@ namespace HeaderArrayConverter
             return new KeySequence<T>(value);
         }
 
+        private IEnumerable<T> Yield(IEnumerable<int> index)
+        {
+            foreach (int i in index)
+            {
+                yield return this[i];
+            }
+        }
+
+        /// <summary>
+        /// Returns a string representation of this instance.
+        /// </summary>
         public override string ToString()
         {
             return _values.Aggregate(string.Empty, (current, next) => $"{current}[{next}]");
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// An enumerator that can be used to iterate through the collection.
+        /// </returns>
         public IEnumerator<T> GetEnumerator()
         {
             return _values?.GetEnumerator() ?? Enumerable.Empty<T>().GetEnumerator();
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="IEnumerator"/> object that can be used to iterate through the collection.
+        /// </returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
 
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">
+        /// An object to compare with this object.
+        /// </param>
+        /// <returns>
+        /// True if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+        /// </returns>
         public bool Equals(KeySequence<T> other)
         {
             return _values.SequenceEqual(other);
         }
 
+        /// <summary>
+        /// Determines whether an object is structurally equal to the current instance.
+        /// </summary>
+        /// <param name="other">
+        /// The object to compare with the current instance.
+        /// </param>
+        /// <param name="comparer">
+        /// An object that determines whether the current instance and <paramref name="other"/> are equal.
+        /// </param>
+        /// <returns>
+        /// True if the two objects are equal; otherwise, false.
+        /// </returns>
         public bool Equals(object other, IEqualityComparer comparer)
         {
             return comparer.Equals(this, other);
