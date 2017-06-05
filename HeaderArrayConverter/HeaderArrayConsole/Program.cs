19d32d06e1f5b06af6b7ba730b3472bd55c0cdb4
Austin Drenski <austin.drenski@gmail.com>
2017-06-05 12:23:55 -0400

    .

--- HeaderArrayConverter/HeaderArrayConsole/Program.cs
+++ HeaderArrayConverter/HeaderArrayConsole/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using HeaderArrayConverter;
-// ReSharper disable UnusedVariable
 
 namespace HeaderArrayConsole
 {
@@ -15,42 +15,9 @@ namespace HeaderArrayConsole
             const string file = "G:\\data\\Austin D\\GTAP source code\\basedata.har";
             //const string file = "G:\\data\\Austin D\\GTAP source code\\sets.har";
 
-            // Reads the full contents of a HAR file into memory.
             HeaderArrayFile arrays = HeaderArrayFile.Read(file);
             
-            // The HeaderArrayFile type handles the formatting of internal objects. Just pass it to the standard output stream.
             Console.WriteLine(arrays);
-
-            // Header arrays are indexed by the 4-character header.
-            IHeaderArray tvom = arrays["TVOM"];
-
-            // This array treats its entries as floats. If TVOM is a string array, an error is thrown.
-            IHeaderArray<float> tvomWithType = arrays["TVOM"].As<float>();
-
-            // This subset contains the untyped entries of TVOM where the first set element is "AGR".
-            ImmutableSequenceDictionary<string, object> subset = tvom["AGR"];
-
-            // This subset contains the typed entries of TVOM where the first set element is "AGR", and the second set element is "USA".
-            ImmutableSequenceDictionary<string, float> subsetWithType = tvomWithType["AGR", "USA"];
-
-            // Subsets handle their own formatting, so just pass one to the standard output to print to the console.
-            Console.WriteLine(subset);
-            Console.WriteLine(subsetWithType);
-
-            // This will subset the first subset for entries where the first set element is "AGR" and the second is "USA".
-            ImmutableSequenceDictionary<string, object> subsetFromSubset = subset["AGR", "USA"];
-
-            // This just creates a clone of subsetWithType because it only contains elements defined by "AGR" and "USA".
-            ImmutableSequenceDictionary<string, float> subsetFromSubsetWithType = subsetWithType["AGR", "USA"];
-
-            // If we know that the subset is fully specified, then .Single() returns a KeyValuePair<string, float> and .Value accesses the float value.
-            float a = (float)subsetFromSubset.Single().Value;
-            float b = subsetFromSubsetWithType.Single().Value;
-
-            // The preceding steps can be combined into a chained call.
-            float c = (float) arrays["TVOM"]["AGR"]["AGR", "USA"].Single().Value;
-            float d = arrays["TVOM"].As<float>()["AGR", "USA"].Single().Value;
-
 
             Console.WriteLine(arrays["TVOM"]["AGR", "USA"]);
 
@@ -89,6 +56,47 @@ namespace HeaderArrayConsole
             }
 
             Console.ReadLine();
+        }
+
+        [SuppressMessage("ReSharper", "UnusedVariable")]
+        [SuppressMessage("ReSharper", "UnusedMember.Local")]
+        private static void Example(string file)
+        {
+            // Reads the full contents of a HAR file into memory.
+            HeaderArrayFile arrays = HeaderArrayFile.Read(file);
+
+            // The HeaderArrayFile type handles the formatting of internal objects. Just pass it to the standard output stream.
+            Console.WriteLine(arrays);
+
+            // Header arrays are indexed by the 4-character header.
+            IHeaderArray tvom = arrays["TVOM"];
+
+            // This array treats its entries as floats. If TVOM is a string array, an error is thrown.
+            IHeaderArray<float> tvomWithType = arrays["TVOM"].As<float>();
+
+            // This subset contains the untyped entries of TVOM where the first set element is "AGR".
+            ImmutableSequenceDictionary<string, object> subset = tvom["AGR"];
+
+            // This subset contains the typed entries of TVOM where the first set element is "AGR", and the second set element is "USA".
+            ImmutableSequenceDictionary<string, float> subsetWithType = tvomWithType["AGR", "USA"];
+
+            // Subsets handle their own formatting, so just pass one to the standard output to print to the console.
+            Console.WriteLine(subset);
+            Console.WriteLine(subsetWithType);
+
+            // This will subset the first subset for entries where the first set element is "AGR" and the second is "USA".
+            ImmutableSequenceDictionary<string, object> subsetFromSubset = subset["AGR", "USA"];
+
+            // This just creates a clone of subsetWithType because it only contains elements defined by "AGR" and "USA".
+            ImmutableSequenceDictionary<string, float> subsetFromSubsetWithType = subsetWithType["AGR", "USA"];
+
+            // If we know that the subset is fully specified, then .Single() returns a KeyValuePair<string, float> and .Value accesses the float value.
+            float a = (float)subsetFromSubset.Single().Value;
+            float b = subsetFromSubsetWithType.Single().Value;
+
+            // The preceding steps can be combined into a chained call.
+            float c = (float)arrays["TVOM"]["AGR"]["AGR", "USA"].Single().Value;
+            float d = arrays["TVOM"].As<float>()["AGR", "USA"].Single().Value;
         }
     }
 }
