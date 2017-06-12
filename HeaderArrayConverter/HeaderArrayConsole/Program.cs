275d3f96a8639efced06a18cb739ca04d65e8254
Austin Drenski <austin.drenski@gmail.com>
2017-06-12 08:40:34 -0400

    Testing on dev station

--- HeaderArrayConverter/HeaderArrayConsole/Program.cs
+++ HeaderArrayConverter/HeaderArrayConsole/Program.cs
@@ -7,17 +7,17 @@ namespace HeaderArrayConsole
     {
         public static void Main()
         {
-            const string directory = "c:\\users\\adren\\desktop\\gtap source";
-            //const string directory = "g:\\data\\austin d\\gtap source code";
+            //const string directory = "c:\\users\\adren\\desktop\\gtap source";
+            const string directory = "g:\\data\\austin d\\gtap source code";
 
             //string input = $"{directory}\\sets.har";
-            string input = $"{directory}\\basedata.har";
+            //string input = $"{directory}\\basedata.har";
             //string input = $"{directory}\\isep.har";
-            //string input = $"{directory}\\gsddat.har";
+            string input = $"{directory}\\gsddat.har";
             //string input = $"{directory}\\samdata.har";
 
-            string jsonOutput = $"{directory}\\test3.harx";
-            string binaryOutput = $"{directory}\\test3.har";
+            string jsonOutput = $"{directory}\\test4.harx";
+            string binaryOutput = $"{directory}\\test4.har";
 
             HeaderArrayFile arrays = HeaderArrayFile.BinaryReader.Read(input);
 
