e1aa9468145f51bf00f367257cda99c100b82aae
Austin Drenski <austin.drenski@gmail.com>
2017-06-14 00:21:57 -0400

    .

---
+++ HeaderArrayConverter/ViewHARX/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ViewHARX
+{
+    [PublicAPI]
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost host = 
+                new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
