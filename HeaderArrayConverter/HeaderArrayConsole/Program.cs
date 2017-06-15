8229f334a18cdc9ca505be8fbe79c97b6485088d
Austin Drenski <austin.drenski@gmail.com>
2017-06-14 20:29:48 -0400

    .

--- HeaderArrayConverter/HeaderArrayConsole/Program.cs
+++ HeaderArrayConverter/HeaderArrayConsole/Program.cs
@@ -6,15 +6,16 @@ namespace HeaderArrayConsole
     {
         public static void Main()
         {
-            //const string directory = "c:\\users\\adren\\desktop\\gtap source";
-            const string directory = "g:\\data\\austin d\\gtap source code";
+            const string directory = "c:\\users\\adren\\desktop\\gtap source\\har testing files";
+            //const string directory = "g:\\data\\austin d\\gtap source code\\har testing files";
 
             //string input = $"{directory}\\sets original.har";
             //string input = $"{directory}\\basedata original.har";
-            //string input = $"{directory}\\isep original.har";
             //string input = $"{directory}\\laborfd original.sl4";
             //string input = $"{directory}\\laborfd original.sol";
-            string input = $"{directory}\\reBAS11 original.sl4";
+            //string input = $"{directory}\\reBAS11\\reBAS11 (har) original.har";
+            //string input = $"{directory}\\reBAS11\\reBAS11 (slc) original.slc";
+            string input = $"{directory}\\reBAS11\\reBAS11 (sl4) original.sl4";
 
             string jsonOutput = $"{directory}\\test6.harx";
             string binaryOutput = $"{directory}\\test6.har";
@@ -23,7 +24,7 @@ namespace HeaderArrayConsole
 
             //Console.WriteLine(arrays);
 
-            HeaderArrayFile.JsonWriter.Write(jsonOutput, arrays);
+            //HeaderArrayFile.JsonWriter.Write(jsonOutput, arrays);
 
             HeaderArrayFile.BinaryWriter.Write(binaryOutput, arrays);
 
