fb61ae5856f93e45642965dbe127d1e73e268a22
Austin Drenski <austin.drenski@gmail.com>
2017-06-25 18:29:14 -0400

    .

--- HeaderArrayConverter/HeaderArrayConsole/Program.cs
+++ HeaderArrayConverter/HeaderArrayConsole/Program.cs
@@ -16,10 +16,10 @@ namespace HeaderArrayConsole
             //string input = $"{CurrentDirectory}\\base\\baserate original.har";
             //string input = $"{CurrentDirectory}\\base\\baseview original.har";
 
-            string input = $"{CurrentDirectory}\\laborfd\\laborfd (sl4) original.sl4";
+            //string input = $"{CurrentDirectory}\\laborfd\\laborfd (sl4) original.sl4";
             //string input = $"{CurrentDirectory}\\laborfd\\laborfd (slc) original.slc";
 
-            //string input = $"{CurrentDirectory}\\reBAS11\\reBAS11 (sl4) original.sl4";
+            string input = $"{CurrentDirectory}\\reBAS11\\reBAS11 (sl4) original.sl4";
             //string input = $"{CurrentDirectory}\\reBAS11\\reBAS11 (slc) original.slc";
 
             // 1. Read the input file with SL4 semantics.
